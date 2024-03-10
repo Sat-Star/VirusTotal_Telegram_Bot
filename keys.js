deac20ac7a83402bb5a6aa669fbb0309919a588a
Satyadeep Mishra <106036477+Sat-Star@users.noreply.github.com>
2024-03-10 19:45:52 +0530

    Create keys.js
    added keys.js

---
+++ keys.js
@@ -0,0 +1,6 @@
+// Here we will store our API Keys
+
+const TELEGRAM_API = 'Your telegram bot token extracted from BotFather'
+const VIRUSTOTAL_API = 'Your VirusTotal API key'
+
+module.exports = { TELEGRAM_API, VIRUSTOTAL_API };
